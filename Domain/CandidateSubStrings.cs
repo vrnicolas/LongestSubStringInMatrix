a98042018e5ba1e639d1190a5ae7745a6b7b3cf7
Nicolás <videlariveronicolas@gmail.com>
2022-07-05 22:12:08 -0300

    Add properties to get the longest value at the CandidateSubStrings class

--- Domain/CandidateSubStrings.cs
+++ Domain/CandidateSubStrings.cs
@@ -7,6 +7,8 @@
         //Use a hashset to discard all the duplicate results.
         private HashSet<string> candidateValues { get; set; }
         public string Orientation { get; set; }
+        public string LongestValue { get { return this.candidateValues.OrderByDescending(x => x.Length).First(); } }
+        public int LongestValueLength { get { return this.LongestValue.Length; } }
 
         public CandidateSubStrings(string orientation)
         {
