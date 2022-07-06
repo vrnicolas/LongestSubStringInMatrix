f7d636fab2e3fc30a36bbb08fd1402fb4d8d82e2
Nicolás <videlariveronicolas@gmail.com>
2022-07-05 22:01:07 -0300

    Add a matrix file reader class with a txt file with an example matrix

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
-
+﻿
 namespace Console
 {
+    using System;
     class Program
     {
         static void Main(string[] args)
---
+++ FileReader/MatrixReader.cs
@@ -0,0 +1,15 @@
+﻿namespace FileReader
+{
+    using System.IO;
+    using System.Linq;
+    public static class MatrixReader
+    {
+        public static string[] ReadFile()
+        {
+            var fileLoc = string.Concat(Directory.GetCurrentDirectory(), "\\Matrix.txt");
+
+            var lines = File.ReadLines(fileLoc);
+            return lines.ToArray();
+        }
+    }
+}
