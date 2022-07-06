6a25ee53b31539ec48966ac117918e4ee23b49ad
Nicolás <videlariveronicolas@gmail.com>
2022-07-05 22:08:41 -0300

    Add the main properties of Matrix and CandidateSubStrings

--- Domain/CandidateSubStrings.cs
+++ Domain/CandidateSubStrings.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Domain
+﻿namespace Domain
 {
-    class CandidateSubStrings
+    using System.Collections.Generic;
+    using System.Linq;
+    public class CandidateSubStrings
     {
+        //Use a hashset to discard all the duplicate results.
+        private HashSet<string> candidateValues { get; set; }
+        public string Orientation { get; set; }
+
+        public CandidateSubStrings(string orientation)
+        {
+            this.candidateValues = new HashSet<string>();
+            this.Orientation = orientation;
+        }
     }
 }
--- Domain/Matrix.cs
+++ Domain/Matrix.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Domain
+﻿namespace Domain
 {
-    class Matrix
+    using System;
+    using System.Linq;
+    public class Matrix
     {
+        private string[,] Values { get; set; }
+        public int Size { get; }
+        public Matrix(string[] matrix)
+        {
+            if (matrix is null || matrix.Length == 0) throw new Exception("The matrix cannot be null or empty");
+
+            var width = matrix.Length;
+            var height = matrix.First().Length;
+
+            this.Size = (width == height) ? width :
+            throw new Exception("Invalid Matrix size, must contain the same width than heigth");
+
+            this.Values = this.parseToCharArray(matrix);
+        }
     }
 }
